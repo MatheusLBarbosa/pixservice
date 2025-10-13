21ffffb20e775f9f65eb28eba1cd647c2360191d
Matheus Barbosa <barbosa.matheuslopes@gmail.com>
2025-10-13 06:57:15 -0300

    Initial Commit

---
+++ src/main/java/com/br/pixservice/PixserviceApplication.java
@@ -0,0 +1,13 @@
+package com.br.pixservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/pixservice/PixserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pixservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
